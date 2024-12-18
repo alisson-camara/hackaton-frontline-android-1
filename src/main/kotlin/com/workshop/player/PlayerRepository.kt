976416adba6af4c8d48e7305e7abe01dae63b4fb
Eduardo Misina <eduardo.misina@ab-inbev.com>
2024-12-18 15:31:44 -0300

    Another commit on the wall2

--- src/main/kotlin/com/workshop/player/PlayerRepository.kt
+++ src/main/kotlin/com/workshop/player/PlayerRepository.kt
@@ -5,6 +5,7 @@ import com.workshop.db.PlayerTable
 import com.workshop.db.TaskTable
 import com.workshop.db.suspendTransaction
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.deleteWhere
 
 class PlayerRepository(
@@ -19,8 +20,7 @@ class PlayerRepository(
 
     override suspend fun deletePlayer(playerName: String, roomId: Int): Boolean = suspendTransaction {
         val rowsDeleted = PlayerTable.deleteWhere {
-            (PlayerTable.room eq roomId)
-            (PlayerTable.name eq playerName)
+            (PlayerTable.room eq roomId) and (PlayerTable.name eq playerName)
         }
         rowsDeleted == 1
     }
